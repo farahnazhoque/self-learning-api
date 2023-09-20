8f81172392a9fac7a179e85d303c889eb99baca1
Farahnaz Hoque <fhoque@uci.edu>
2023-09-19 18:23:54 -0700

    added creation of posts

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -14,6 +15,11 @@ class Post(BaseModel): # an extension of the BaseModel class
 # storing information about posts to retrieve   
 my_posts = [{"title": "post 1", "content" : "new content for post 1", "id": 1}, {"title": "fav food", "content": "nuggets", "id" : 2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -22,7 +28,17 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post): # ensuring the schema is validated from the front end
-    print(post.dict())
-    return {"data" : post.dict()}
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 100000)
+    my_posts.append(post_dict)
+    print(my_posts)
+    return {"data" : post_dict}
+
+@app.get("/posts/{id}")
+def get_posts_id(id: int, response: Response): # response is added to tweak the response anyway we want it
+    post = find_post(id) # we have to convert it to int as it is passed as str
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id : {id} not found")
+    return {"post details" : post}
